c50da74b54e7eac2a1df2f0a457a47f4d5250bb9
cmps253qflow <cmps253qflow@gmail.com>
2017-03-19 19:56:42 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,32 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('notifyNewAnswer', function(req, res) {
+  
+     var params = req.params;
+     var selectedUsers = req.params.selectedUser
+ 
+     var data = params.data
+  
+  
+ var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.containedIn('username', selectedUsers);
+  
+  Parse.Push.send({
+  where: pushQuery,
+  data: {
+    alert: data,
+    badge: 1,
+    sound: 'default'
+  }
+}, {
+  useMasterKey: true,
+  success: function() {
+    // Push sent!
+  },
+  error: function(error) {
+    // There was a problem :(
+  }
+});
+});
