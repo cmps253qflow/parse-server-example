13f1451091d77911bb0326a328ceb29c972b0499
cmps253qflow <cmps253qflow@gmail.com>
2017-03-29 04:05:13 +0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,6 +8,10 @@ Parse.Cloud.define('notifyNewAnswer', function(req, res) {
   console.log("We are in");
      var params = req.params;
      var selectedUsers = req.params.selectedUser
+     
+     var notiData = req.params.notiData
+     var theUser = req.params.theUser
+     var theUserData = req.params.theUserData
  
      var data = params.data
   
@@ -32,6 +36,48 @@ Parse.Cloud.define('notifyNewAnswer', function(req, res) {
     res.error();
   }
 });
+	
+	 var userQuery = new Parse.Query('_User');
+  userQuery.containedIn('username', selectedUsers);
+	 userQuery.find({
+  success: function(results) {
+ 
+  
+ 
+
+   for (var i = 0; i < results.length; i++) {
+  
+	   
+	   
+	
+    var userData = results[i];
+	   var urs = userData.get('username');
+	   if(urs === theUser){
+		     userData.add('notifications',theUserData);
+	   }else{
+	   	     userData.add('notifications',notiData);
+	   }
+   
+    userData.save(null, { useMasterKey: true });
+	  
+    
+     
+   }
+    res.success('I passed on ');
+   
+     
+  
+  },
+
+  error: function(error) {
+    // error is an instance of Parse.Error.
+  }
+});
+	
+	
+	
+	
+	
 });
 
 Parse.Cloud.define('incrementRep',function(request,response)
